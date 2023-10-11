c4b62618f98d729539d88938ae051ef90bafd44d
david1opez <david1opez@outlook.com>
2023-10-11 15:04:08 -0600

    Create Icon component

--- src/app/doc/components/LogMakerComponents.tsx
+++ src/app/doc/components/LogMakerComponents.tsx
@@ -2,11 +2,13 @@ import PagePreview from "./PagePreview";
 import Searchbar from "./Searchbar";
 import AddPageButton from "./AddPageButton";
 import DragComponent from "./DragComponent";
+import Icon from "./Icon";
 
 import SimpleDroppable from "./dnd/SimpleDroppable";
 import SimpleDraggable from "./dnd/SimpleDraggable";
 
 export {
+    Icon,
     PagePreview,
     Searchbar,
     AddPageButton,
