1943094e3cc587e52f89bf4be33bae1b26a07e5a
david1opez <david1opez@outlook.com>
2023-10-11 14:38:39 -0600

    Centralize component imports

---
+++ src/app/doc/components/LogMakerComponents.tsx
@@ -0,0 +1,16 @@
+import PagePreview from "./PagePreview";
+import Searchbar from "./Searchbar";
+import AddPageButton from "./AddPageButton";
+import DragComponent from "./DragComponent";
+
+import SimpleDroppable from "./dnd/SimpleDroppable";
+import SimpleDraggable from "./dnd/SimpleDraggable";
+
+export {
+    PagePreview,
+    Searchbar,
+    AddPageButton,
+    DragComponent,
+    SimpleDroppable,
+    SimpleDraggable
+};
